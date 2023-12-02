e9e7280025262b30712da45ac343104c6bc55479
tmayolgomila <tmayolgomila@gmail.com>
2023-12-02 17:27:12 +0100

    work on metrics

--- src/components/myProgress.jsx
+++ src/components/myProgress.jsx
@@ -1,5 +1,5 @@
-// En el componente MyProgress
 import React, { useState, useEffect } from 'react';
+import '../styles/myProgress.css'
 
 const MyProgress = () => {
   const [oldWorkout, setOldWorkout] = useState({});
@@ -9,19 +9,68 @@ const MyProgress = () => {
     setOldWorkout(storedOldWorkout);
   }, []);
 
+  const calculateMetric = (exercise) => {
+    if (exercise.weight > 0) {
+      // Si hay peso especificado, calcular la Carga Total Levantada
+      return exercise.weight * exercise.repetitions * exercise.sets;
+    } else {
+      // Si no hay peso o es 0, calcular el Volumen de Entrenamiento (Reps x Sets)
+      return exercise.repetitions * exercise.sets;
+    }
+  };
+
+  const organizeByExercise = (exercises) => {
+    // Organiza los ejercicios por el nombre del ejercicio
+    const organizedByExercise = {};
+    exercises.forEach((exercise) => {
+      const exerciseName = exercise.exercise || 'Ejercicio Sin Especificar'; // Usa 'Ejercicio Sin Especificar' si no se especifica
+      if (!organizedByExercise[exerciseName]) {
+        organizedByExercise[exerciseName] = [];
+      }
+      organizedByExercise[exerciseName].push(exercise);
+    });
+    return organizedByExercise;
+  };
+
+const saveMetricToLocalStorage = (exerciseName, metric) => {
+  const storedMetrics = JSON.parse(localStorage.getItem('exerciseMetrics')) || {};
+  if (!storedMetrics[exerciseName]) {
+    storedMetrics[exerciseName] = [];
+  }
+
+  // Verificar si la métrica ya existe para este ejercicio antes de agregarla
+  const existingMetricIndex = storedMetrics[exerciseName].findIndex((storedMetric) => storedMetric === metric);
+  if (existingMetricIndex === -1) {
+    storedMetrics[exerciseName].push(metric);
+    localStorage.setItem('exerciseMetrics', JSON.stringify(storedMetrics));
+  }
+};
+
+
   return (
-    <div>
+    <div className='myProgressContainer'>
       <h2>My Progress</h2>
       {Object.entries(oldWorkout).map(([trainingName, exercises], index) => (
         <div key={index}>
           <h3>{trainingName}</h3>
-          <ul>
-            {exercises.map((exercise, exerciseIndex) => (
-              <li key={exerciseIndex}>
-                {exercise.exercise} - Weight: {exercise.weight}, Reps: {exercise.repetitions}, Sets: {exercise.sets}
-              </li>
-            ))}
-          </ul>
+          {Object.entries(organizeByExercise(exercises)).map(([exerciseName, exerciseExercises], exerciseIndex) => (
+            <div key={exerciseIndex}>
+              <h4>{exerciseName}</h4>
+              <ul className='myProgressList'>
+                {exerciseExercises.map((exercise, innerExerciseIndex) => {
+                  const metric = calculateMetric(exercise);
+                  saveMetricToLocalStorage(exerciseName, metric); // Guardar la métrica en el localStorage
+                  return (
+                    <li key={innerExerciseIndex}>
+                      Weight: {exercise.weight}, Reps: {exercise.repetitions}, Sets: {exercise.sets}
+                      <br />
+                      Metric: <strong>{metric}</strong> {/* Muestra la métrica calculada */}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          ))}
         </div>
       ))}
     </div>
